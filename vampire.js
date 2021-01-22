2323de95c9453108a252d456541f381f0ac06dbe
Ali Bas <alibas01@gmail.com>
2021-01-22 16:53:09 -0500

    no traversal

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,7 +43,25 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let createrArray = [vampire];
+    let createrThis = [this];
+    let currentVampire = vampire.creator;
+      for (let i = 0; i < vampire.numberOfVampiresFromOriginal; i++) {
+        createrArray.push(currentVampire);
+        currentVampire = currentVampire.creator;
+      }
+      currentVampire = this.creator;
+      for (let i = 0; i < this.numberOfVampiresFromOriginal; i++) {
+        createrThis.push(currentVampire);
+        currentVampire = currentVampire.creator;
+      }
+      for (let creater of createrArray) {
+        for (let create of createrThis) {
+          if(creater === create) {
+            return create;
+          }
+        }
+      }
   }
 }
 
